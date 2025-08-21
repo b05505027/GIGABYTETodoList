f37119e43074884ac67b8cdcebb963085ab11d16
b05505027 <b05505027@ntu.edu.tw>
2025-08-21 19:17:41 +0800

    feat: 🎸 add button components; resolve tailwind issues

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,30 +1,18 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://electron-vite.github.io" target="_blank">
-      <img src="/electron-vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <main class="flex flex-col items-center justify-center min-h-screen bg-brand-gray space-y-8">
+    
+    <!-- 1. Default Button (will change on hover) -->
+    <BaseButton>Btn</BaseButton>
+
+    <!-- 2. A static representation of the "Hover" state for comparison -->
+    <BaseButton class="bg-button-hover">Hover</BaseButton>
+    
+    <!-- 3. Disabled Button -->
+    <BaseButton disabled>Disable</BaseButton>
+
+  </main>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup>
+import BaseButton from './components/BaseButton.vue';
+</script>
---
+++ app/src/components/BaseButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <button
+    :disabled="disabled"
+    class="
+      px-6 
+      py-6 
+      rounded-2xl
+      font-sans text-4xl font-light text-brand-black
+      bg-button-normal
+      transition-colors duration-200 ease-in-out
+      focus:outline-none focus:ring-2 focus:ring-offset-2
+      hover:bg-button-hover
+      disabled:bg-button-normal 
+      disabled:text-brand-gray 
+      disabled:cursor-not-allowed
+      disabled:hover:bg-button-normal
+    "
+  >
+    <slot />
+  </button>
+</template>
+
+<script setup>
+defineProps({
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
