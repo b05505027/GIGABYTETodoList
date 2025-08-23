c3e7130204b9af977900021535438ebf88a805f0
b05505027 <b05505027@ntu.edu.tw>
2025-08-23 17:06:47 +0800

    feat: 🎸 build content router

---
+++ app/src/router/index.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import ItemDetail from '../components/ItemDetail.vue';
+import EmptyState from '../components/EmptyState.vue';
+
+const routes = [
+  { 
+    path: '/', 
+    redirect: '/content' // Redirect root to a default view
+  },
+  {
+    path: '/content',
+    component: EmptyState, // Component for when no item is selected
+  },
+  {
+    path: '/content/:id',
+    component: ItemDetail,
+    props: true, // Automatically pass route params as props
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
